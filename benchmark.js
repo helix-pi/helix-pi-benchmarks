ce57ff82f945561606d959bc033dd8d0b3d0eb04
Nick Johnstone <ncwjohnstone@gmail.com>
2015-08-22 14:42:10 +1200

    I now have a command line tool for running benchmarks, closes #1

---
+++ benchmark.js
@@ -0,0 +1,24 @@
+var glob = require('glob');
+
+function displayBenchmark (benchmark) {
+  return function (filename) {
+    console.log(filename);
+
+    benchmark(filename);
+
+    console.log('\n');
+  };
+}
+
+function requireFile (filename) {
+  return require(filename);
+}
+
+function stripExtension (filename) {
+  return filename.replace('.js', '');
+}
+
+glob('./benchmarks/**/*.js', function (er, files) {
+  files.map(stripExtension).forEach(displayBenchmark(requireFile));
+});
+
